7d3e0bf06abc811d2be055c9d7d6c64a912e0937
Florian LE GALOUDEC <pti.breton@hotmail.fr>
2024-03-26 23:00:17 +0100

    test sans le slash

--- app/index.app.js
+++ app/index.app.js
@@ -8,7 +8,7 @@ import createDoc from "./helpers/swagger.doc.js";
 const app = express();
 
 app.use(cors({
-  origin: "https://confection-main.netlify.app/",
+  origin: "https://confection-main.netlify.app",
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "application/json"],
   credentials: true,
